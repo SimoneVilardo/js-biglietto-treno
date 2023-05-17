c0469afe512a6b37c1e7c8fa1ab5c32267174071
SimoneVilardo <simovila95@gmail.com>
2023-05-17 17:05:49 +0200

    Ultime modifiche Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -1,42 +1,36 @@
 //chiedere all'utente quanti chilometri dovrà percorrere
 let kilometri = parseInt(prompt('Quanti kilometri dovrai percorrere?').trim());
-console.log(kilometri)
 
 // costante prezzo base
-const costo_base = 0.21
+const costo_base = 0.21;
 
 // totale biglietto
-let totale = (kilometri * costo_base).toFixed(2)
-console.log(totale)
+let totale = (kilometri * costo_base).toFixed(2);
 
 //eta utente
-let eta = parseInt(prompt('Quanti anni hai?').trim())
+let eta = parseInt(prompt('Quanti anni hai?').trim());
 
-let sconto 
+let sconto;
 
-let prezzo_scontato
+let prezzo_scontato;
 
 
 // condizioni
 if (eta < 18) {
-    sconto = (totale * 20 / 100)
-    console.log(sconto)
+    sconto = (totale * 20 / 100);
 
-    prezzo_scontato = (totale - sconto).toFixed(2)
-    console.log(prezzo_scontato)
+    prezzo_scontato = (totale - sconto).toFixed(2);
 
-    document.getElementById('prezzo_finale').innerHTML = prezzo_scontato
+    document.getElementById('prezzo_finale').innerHTML = prezzo_scontato;
 
 } else if ( eta >= 65) {
-        sconto = (totale * 40 / 100)
-        console.log(sconto)
+        sconto = (totale * 40 / 100);
     
-        prezzo_scontato = (totale - sconto).toFixed(2)
-        console.log(prezzo_scontato)
+        prezzo_scontato = (totale - sconto).toFixed(2);
 
-        document.getElementById('prezzo_finale').innerHTML = prezzo_scontato
+        document.getElementById('prezzo_finale').innerHTML = prezzo_scontato;
 } else {
-    document.getElementById('prezzo_finale').innerHTML = totale
+    document.getElementById('prezzo_finale').innerHTML = totale;
 }
 
 // risultato in pagina
